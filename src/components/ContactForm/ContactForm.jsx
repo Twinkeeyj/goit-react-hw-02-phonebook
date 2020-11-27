1d37141d4df2ec4835f51861c990532f461266a7
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-11-27 11:24:13 +0200

    “edits”

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import classes from "./ContactForm.module.css"
+import classes from './ContactForm.module.css';
 import { v4 as uuidv4 } from 'uuid';
-
 
 const INITIAL_STATE = {
   name: '',
@@ -13,7 +12,7 @@ export default class ContactForm extends Component {
   state = { ...INITIAL_STATE };
 
   handleChange = ({ target }) => {
-    const { name } = this.state;
+    // const { name } = this.state;
     this.setState({
       id: uuidv4(),
       [target.name]: target.value,
@@ -21,24 +20,45 @@ export default class ContactForm extends Component {
   };
   handleSubmit = event => {
     event.preventDefault();
-    const { name } = this.state;
     this.props.addContact({ ...this.state });
+    this.setState({
+      name: '',
+      number: '',
+    });
   };
 
   render() {
     const { name, number } = this.state;
     return (
-      < >
-        <form className={classes.container} action="submit" onSubmit={this.handleSubmit}>
-          <label className={classes.label}  htmlFor="name" placeholder="name" value={name}>
+      <>
+        <form
+          className={classes.container}
+          action=""
+          onSubmit={this.handleSubmit}
+        >
+          <label className={classes.label} htmlFor="name">
             Name
           </label>
-          <input className={classes.input} type="text" name="name" onChange={this.handleChange} />
+          <input
+            className={classes.input}
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={this.handleChange}
+          />
 
-          <label className={classes.label} htmlFor="number" placeholder="number" value={number}>
+          <label className={classes.label} htmlFor="number">
             Number
           </label>
-          <input className={classes.input} type="tel" name="number" onChange={this.handleChange} />
+          <input
+            className={classes.input}
+            type="tel"
+            name="number"
+            placeholder="number"
+            value={number}
+            onChange={this.handleChange}
+          />
           <input type="submit" value="Add contact" />
         </form>
       </>
